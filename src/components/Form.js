bd288f8d4bb54f590c8857a4d7a5990b0693bee2
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-04-29 14:25:24 +0300

    Commit changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,47 @@
-import React from "react";
+import React,{useState} from "react";
+import Button from "./Button";
 
-function Form() {
 
-    function addTransaction(event) {
+function Form({transactions, setTransactions}) {
+    const [newTransactions, setNewTransactions] = useState({
+        date: "",
+        description: "",
+        category: "",
+        amount: ""
+})
+
+    function handleSubmit(event) {
         event.preventDefault()
+        setTransactions([...transactions, newTransactions])
         
-        
+    }
+
+    function handleChange(event){
+        setNewTransactions(
+            {
+                ...newTransactions,
+                [event.target.name] : event.target.value
+            }
+        )
+
     }
 
     return (
         <div className="container-fluid">
-        <form className="row">
+        <form className="row" onSubmit={handleSubmit}>
             <div className="col-lg-3 col-md-4 col-sm-6">
-                <input className="form-control mb-2" type="date" placeholder=""/>
-            </div>
-            <div className="col-lg-3 col-md-4 col-sm-6">
-                <input className="form-control mb-2" type="text" placeholder="Description"/>
+                <input className="form-control mb-2" type="date" onChange={handleChange} value={newTransactions.date} placeholder="" name="date"/>
             </div>
             <div className="col-lg-3 col-md-4 col-sm-6">
-                <input className="form-control mb-2" type="text" placeholder="Category"/>
+                <input className="form-control mb-2" type="text" onChange={handleChange} value={newTransactions.description} placeholder="Description" name="description"/>
             </div>
             <div className="col-lg-3 col-md-4 col-sm-6">
-                <input className="form-control mb-2" type="text" placeholder="Amount"/>
+                <input className="form-control mb-2" type="text" onChange={handleChange} value={newTransactions.category} placeholder="Category" name="category"/>
             </div>
-            <div>
-                <button type="submit" className="btn btn-outline-primary btn-lg" onClick={addTransaction}>Add Transaction</button>
+            <div className="col-lg-3 col-md-4 col-sm-6">
+                <input className="form-control mb-2" type="text" onChange={handleChange} value={newTransactions.amount} placeholder="Amount" name="amount"/>
             </div>
+            <Button/>
         </form>
     </div>
 
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,64 +1,55 @@
 import React, {useState} from "react";
+import Search from "./Search";
 import Form from "./Form";
 import Row from "./Row";
 
-function Table() {
+const myTransactions = [
+    {
+        date: "2024-01-05",
+        description: "Paycheck from YouTube",
+        category: "Income",
+        amount: "50,000.00"
 
-    const [transactions, setTransactions] = useState([
-        {
-            date: "2024-01-05",
-            description: "Paycheck from YouTube",
-            category: "Income",
-            amount: "50,000.00"
+    },
+    {
+        date: "2024-03-29",
+        description: "Phillips Airfryer",
+        category: "Electronics",
+        amount: "5,000.00"
 
-        },
-        {
-            date: "2024-03-29",
-            description: "Phillips Airfryer",
-            category: "Electronics",
-            amount: "5,000.00"
+    },
+    {
+        date: "2023-12-11",
+        description: "Shopping Spree at Harrods",
+        category: "Fashion",
+        amount: "125,000.00"
 
-        },
-        {
-            date: "2023-12-11",
-            description: "Shopping Spree at Harrods",
-            category: "Fashion",
-            amount: "125,000.00"
+    },
+    {
+        date: "2023-12-31",
+        description: "Uber Payments",
+        category: "Transportation",
+        amount: "10,000.00"
 
-        },
-        {
-            date: "2023-12-31",
-            description: "Uber Payments",
-            category: "Transportation",
-            amount: "10,000.00"
-
-        },
-        {
-            date: "2023-12-17",
-            description: "Burna Boy's London concert",
-            category: "Entertainment",
-            amount: "30,000.00"
-
-        }
-    ]
-)
-
-    function filterItems(event){
-        const change = event.target.value
-        const filter = transactions.filter(transactions => {
-            return transactions.category.toLowerCase().includes(change.toLowerCase())
-        })
-
-        setTransactions([...filter])
+    },
+    {
+        date: "2023-12-17",
+        description: "Burna Boy's London concert",
+        category: "Entertainment",
+        amount: "30,000.00"
 
     }
+]
+
+
+function Table() {
+   
+    const [transactions, setTransactions] = useState(myTransactions)
 
     return (
         <>
-            <div className="p-3">
-                <input onChange={filterItems} className="form-control me-2" type="search" placeholder="Search for Recent Transactions"/>
-            </div>
-            <Form/>
+            <Search transactionArr={myTransactions} transactions={transactions} setTransactions={setTransactions} />
+            <Form transactions={transactions} setTransactions={setTransactions}/>
             <table className="table table-bordered table-striped">
                 <thead>
                     <tr>
